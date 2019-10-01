b7a92fbe149016ee6ea00cc0ead9baa2c56c799e
zhaoshuhong <2459636918@qq.com>
2019-10-01 21:01:34 +0800

    修改cli

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const inquirer = require('inquirer');
 const ora = require('ora');
 const chalk = require('chalk');
 const symbols = require('log-symbols');
+const exec = require('child_process').exec;
+const config = require('./config.json');
+let branchM = config.tpl.mcrm.url;
+let n = branchM;
+n = n.split('/');
+n = n[n.length-1].split('.')[0];
 
 program.version('1.0.0', '-v, --version')
     .command('init <name>')
@@ -25,30 +31,77 @@ program.version('1.0.0', '-v, --version')
                 {
                     name: 'author',
                     message: '请输入作者名称'
+                },
+                {
+                    type: 'list',
+                    message: '请选择一种安装:',
+                    name: 'type',
+                    choices: [
+                        "gulp+项目",
+                        "只安装gulp",
+                        "只拉取文件"
+                    ],
+                    // filter: function (val) { // 使用filter将回答变为小写
+                    //     return val.toLowerCase();
+                    // }
                 }
             ]).then((answers) => {
-                const spinner = ora('正在下载模板...');
-                spinner.start();
-                download('zhaoshuhong/home-template', name, (err) => {
-                    if (err) {
-                        spinner.fail();
-                        console.log(symbols.error, chalk.red(err));
-                    } else {
-                        spinner.succeed();
-                        const fileName = `${name}/package.json`;
-                        const meta = {
-                            name,
-                            description: answers.description,
-                            author: answers.author
+
+                if(answers.type == 'gulp+项目' || answers.type == 'gulp'){
+                    const spinner = ora('正在下载模板...');
+                    spinner.start();
+                    // download('zhaoshuhong/home-template', name, (err) => { github
+                    download(`direct:${config.tpl.gulp.url}`, name, {clone: true}, (err) => {
+                        if (err) {
+                            spinner.fail();
+                            console.log(symbols.error, chalk.red(err));
+                        } else {
+                            spinner.succeed();
+                            const fileName = `${name}/package.json`;
+                            const meta = {
+                                name,
+                                description: answers.description,
+                                author: answers.author
+                            }
+                            if (fs.existsSync(fileName)) {
+                                const content = fs.readFileSync(fileName).toString();
+                                const result = handlebars.compile(content)(meta);
+                                fs.writeFileSync(fileName, result);
+                            }
+                            console.log(symbols.success, chalk.green('gulp安装成功'));
+                            // git命令，远程拉取项目并自定义项目名
+                            console.log('开始拉取项目 git >>>>>> ');
+                            if(answers.type == 'gulp+项目'){
+                                let cmdStr = `cd ${name} && git clone ${config.tpl.mcrm.url} && cd ${n} && git checkout ${config.tpl.mcrm.branch}`
+                                exec(cmdStr, (error, stdout, stderr) => {
+                                    if (error) {
+                                        console.log('错误---',error)
+                                        process.exit()
+                                    }
+                                    console.log(chalk.green('\n √ Generation completed!'))
+                                    console.log(`\n cd ${name} && npm gulp -g && npm install 安装gulp依赖\n`)
+                                    process.exit()
+                                })
+                            }else{
+                                console.log(`\n cd ${name} && npm install 安装gulp依赖\n`)
+                            }
+
+
+
                         }
-                        if (fs.existsSync(fileName)) {
-                            const content = fs.readFileSync(fileName).toString();
-                            const result = handlebars.compile(content)(meta);
-                            fs.writeFileSync(fileName, result);
+                    })
+                }else{
+                    let cmdStr = `cd ${name} && git clone ${config.tpl.mcrm.url} && && cd ${n} && git checkout ${config.tpl.mcrm.branch}`
+                    exec(cmdStr, (error, stdout, stderr) => {
+                        if (error) {
+                            console.log('错误---',error)
+                            process.exit()
                         }
-                        console.log(symbols.success, chalk.green('项目初始化完成'));
-                    }
-                })
+                        console.log(chalk.green('\n √ Generation completed!'))
+                        process.exit()
+                    })
+                }
+
             })
         } else {
             // 错误提示项目已存在，避免覆盖原有项目
