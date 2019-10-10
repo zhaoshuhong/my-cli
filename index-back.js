21d797cff3b69eb73bea05d0613b48306937f2ba
zhaoshuhong <2459636918@qq.com>
2019-10-10 15:29:49 +0800

    更新

--- index-back.js
+++ index-back.js
@@ -15,43 +15,6 @@ const symbols = require('log-symbols');
 const exec = require('child_process').exec;
 const config = require('./config.json');
 
-let branchM = config.tpl.pro;
-
-let spinner = ora('正在下载模板...');
-
-function getName(url) {
-    url = url || '';
-    if (url) {
-        let n = url;
-        n = n.split('/');
-        n = n[n.length - 1].split('.')[0];
-        return n;
-    }
-
-}
-
-function getGit(name) {
-    spinner = ora('正在从git拉取代码...');
-    spinner.start();
-    let cmdStr = `cd ${name} `;
-    for (let item in branchM) {
-        let url = getName(branchM[item].url)
-        cmdStr += `&& git clone ${branchM[item].url} && cd ${url} && git checkout ${branchM[item].branch} && cd ../ `
-    }
-    exec(cmdStr, (error, stdout, stderr) => {
-        if (error) {
-            spinner.fail();
-            console.log('错误---', error)
-            process.exit()
-        }
-        spinner.succeed();
-        console.log(chalk.green('\n √ Generation completed!'))
-        console.log(`\n cd ${name} && npm gulp -g && npm install 安装gulp依赖\n`)
-        process.exit()
-    })
-}
-
-
 program.version('1.0.0', '-v, --version')
     .command('init <name>')
     .action((name) => {
@@ -73,18 +36,17 @@ program.version('1.0.0', '-v, --version')
                         "gulp+项目",
                         "只安装gulp",
                         "只拉取文件"
-                    ],
+                        ],
                     // filter: function (val) { // 使用filter将回答变为小写
                     //     return val.toLowerCase();
                     // }
                 }
             ]).then((answers) => {
-
-                if (answers.type == 'gulp+项目' || answers.type == '只安装gulp') {
-                    spinner = ora('正在下载模板...');
-                    spinner.start();
-                    // download('zhaoshuhong/home-template', name, (err) => { github
-                    download(`direct:${config.tpl.tools.gulp.url}`, name, {clone: true}, (err) => {
+                const spinner = ora('正在下载模板...');
+                spinner.start();
+                // download('zhaoshuhong/home-template', name, (err) => { github
+                if(answers.type == 'gulp+项目' || answers.type == 'gulp'){
+                    download(`direct:${config.tpl.gulp.url}`, name, {clone: true}, (err) => {
                         if (err) {
                             spinner.fail();
                             console.log(symbols.error, chalk.red(err));
@@ -101,28 +63,15 @@ program.version('1.0.0', '-v, --version')
                                 const result = handlebars.compile(content)(meta);
                                 fs.writeFileSync(fileName, result);
                             }
-                            console.log(symbols.success, chalk.green('gulp安装成功'));
-                            // git命令，远程拉取项目并自定义项目名
-                            if (answers.type == 'gulp+项目') {
-                                getGit(name)
-                            } else {
-                                console.log(`\n cd ${name} && npm install 安装gulp依赖\n`)
+                            console.log(symbols.success, chalk.green('gulp安装'));
+                            if(answers.type == 'gulp+项目'){
+                                gitPull()
                             }
+                           console.log(`\n cd ${name} && npm install 安装gulp依赖\n`)
                         }
                     })
-                } else {
-                    // console.log('创建文件')
-                    // fs.writeFileSync('README.md','test');   //先写入一个空的文件 创建目录 name
-                    // console.log('创建文件end')
-                    fs.mkdir(name, function (err) {
-                        if (err) {
-                            console.log("mkdir err:", err);
-                            return;
-                        }
-                        console.log("mkdir create success");
-                        getGit(name)
-                    })
-
+                }else{
+                    gitPull()
                 }
 
             })
@@ -131,4 +80,17 @@ program.version('1.0.0', '-v, --version')
             console.log(symbols.error, chalk.red('项目已存在'));
         }
     })
+function gitPull(){
+    // git命令，远程拉取项目并自定义项目名
+    console.log('开始拉取项目 git >>>>>> ')
+    let cmdStr = `cd ${name} && git clone ${config.tpl.mcrm.url} && checkout ${config.tpl.branch}`
+    exec(cmdStr, (error, stdout, stderr) => {
+        if (error) {
+            console.log('错误---', error)
+            process.exit()
+        }
+        console.log(chalk.green('\n √ Generation completed!'))
+        process.exit()
+    })
+}
 program.parse(process.argv);
